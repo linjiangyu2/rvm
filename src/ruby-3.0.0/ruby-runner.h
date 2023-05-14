074c2db7a23ce6d617e9387437c2b81c1835a0ae
Tianlin <linjiangyu0702@linjiangyu.com>
2023-05-14 12:10:20 +0800

    initial

---
+++ src/ruby-3.0.0/ruby-runner.h
@@ -0,0 +1,9 @@
+#define ABS_SRCDIR "/usr/local/rvm/src/ruby-3.0.0"
+#define BUILDDIR   "/usr/local/rvm/src/ruby-3.0.0"
+#define LIBPATHENV "LD_LIBRARY_PATH"
+#define PRELOADENV "LD_PRELOAD"
+#define PATH_SEPARATOR ":"
+#define PATH_SEP   ':'
+#define EXTOUT     ".ext"
+#define ARCH       "x86_64-linux"
+#define SOEXT      "so"
